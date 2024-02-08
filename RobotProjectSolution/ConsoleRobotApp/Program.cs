d600f5c7dde9fa604adb1dd21ced7807a0e698cf
Anton Cheremushkin <96653314+AntoniO4333@users.noreply.github.com>
2024-02-08 10:34:28 +0700

    Add files via upload

---
+++ RobotProjectSolution/ConsoleRobotApp/Program.cs
@@ -0,0 +1,46 @@
+﻿using RobotLibrary2;
+using System;
+using System.Threading;
+
+namespace RobotLibrary
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            Robot[] robots = new Robot[3];
+
+            // 3 робота
+            for (int i = 0; i < 3; i++)
+            {
+                robots[i] = new Robot(0, 0);
+            }
+
+            // Создаем потоки для каждого робота
+            Thread[] threads = new Thread[3];
+            for (int i = 0; i < 3; i++)
+            {
+                int robotNumber = i + 1; // Номер робота для вывода
+                threads[i] = new Thread(() =>
+                {
+
+                    // 10 шагов на робота
+                    for (int j = 0; j < 10; j++)
+                    {
+                        robots[robotNumber - 1].Move(robotNumber);
+                        Thread.Sleep(100); 
+                    }
+                });
+
+                // номер робота как параметр в замыкание
+                threads[i].Start();
+            }
+
+            // Ожидаем завершения всех потоков
+            foreach (var thread in threads)
+            {
+                thread.Join();
+            }
+        }
+    }
+}
---
+++ RobotProjectSolution/RobotLibrary/Robot.cs
@@ -0,0 +1,83 @@
+﻿
+using System.Drawing;
+
+namespace RobotLibrary
+{
+    /// <summary>
+    /// Класс робота, который двигается в случайном направлении.
+    /// </summary>
+    public class Robot
+    {
+        private (int, int) coord;
+        private int dir;
+        private string name;
+
+        /// <summary>
+        /// Робот с координатами x, y
+        /// </summary>
+        /// <param name="x">движение робота по горизонтали</param>
+        /// <param name="y">движение робота по вертикали</param>
+        public Robot(int x, int y)
+        {
+            coord = (x, y);
+        }
+        /// <summary>
+        /// направление и координаты робота
+        /// </summary>
+        /// <returns>Возвращает направления и координаты робота</returns>
+        public override string ToString()
+        {
+            return $"{DirName(dir)} ({coord.Item1} , {coord.Item2})";
+        }
+
+        /// <summary>
+        /// Направление задается случайно
+        /// </summary>
+        private void RandomizeDirection()
+        {
+            Random random = new Random();
+            dir = random.Next(4); // Ген случ направления (0-3)
+        }
+
+        /// <summary>
+        /// Название направления
+        /// </summary>
+        /// <param name="dir">Переменная, задающая направление</param>
+        /// <returns></returns>
+        private static string DirName(int dir)
+        {
+            switch (dir)
+            {
+                case 0: return "вверх";
+                case 1: return "вправо";
+                case 2: return "вниз";
+                case 3: return "влево";
+                default: return "неопределенное направление";
+            }
+        }
+
+        /// <summary>
+        /// В зависимости от направления dir робот перемещается в определенном направлении
+        /// </summary>
+        public void Move()
+        {
+            RandomizeDirection();
+            switch (dir)
+            {
+                case 0: // Вверх
+                    coord.Item2++;
+                    break;
+                case 1: // Вправо
+                    coord.Item1++;
+                    break;
+                case 2: // Вниз
+                    coord.Item2--;
+                    break;
+                case 3: // Влево
+                    coord.Item1--;
+                    break;
+            }
+            Console.WriteLine(ToString());
+        }  
+    }
+}
